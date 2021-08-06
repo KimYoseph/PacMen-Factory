a1c070857c64f1eced587bbe4839564bd3063fca
Yoseph_Kim <yosephkim10@gmail.com>
2021-08-06 13:17:35 -0700

    Pacmen fixed

--- pacmen.js
+++ pacmen.js
@@ -17,8 +17,8 @@ function setToRandom(scale) {
 // Factory to make a PacMan at a random position with random velocity
 function makePac() {
   // returns an object with random values scaled {x: 33, y: 21}
-  let velocity = setToRandom(500); // {x:?, y:?}
-  let position = setToRandom(500);
+  let velocity = setToRandom(20); // {x:?, y:?}
+  let position = setToRandom(window.innerWidth);
 
   // Add image to div id = game
   let game = document.getElementById('game');
